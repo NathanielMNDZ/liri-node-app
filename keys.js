7d7a7ee12a04ddda9533caf6e456a46a4777af25
Nathaniel <nxm010@gmail.com>
2017-01-18 15:46:20 -0600

    stage 1-setup keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'clAXelCD4kNyHzJll2VSLUctd',
+  consumer_secret: '7QrNB61IE5GqSd9nkGFKFNFaj1WfpEUKVE37MHnTnAQIS7QaCY',
+  access_token_key: '821833952658866177-duPtcJRQefT4y7xp42sYHfDLdABkU3C',
+  access_token_secret: 'U89XXHwuEflOWHDYdRuKLVf4ya2lZzvMeKnVLu4aqxg1I',
+}
